b186b93fcbaa94c67e3fc88335262f1cd5d1083b
TeeNik <teenik3315@gmail.com>
2019-08-11 00:14:51 +0300

    map

---
+++ Assets/Scripts/Core/MapController.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Tilemaps;
+
+public class MapController : MonoBehaviour
+{
+
+    public Tilemap tileMap;
+    public Transform Pointer;
+    public Grid grid;
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        var mouse = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        var tile = grid.WorldToCell(mouse);
+        Pointer.position = tileMap.GetCellCenterWorld(new Vector3Int(tile.x, tile.y, tile.z));
+        Debug.Log($"Pos: x={tile.x} y={tile.y}");
+    }
+}
