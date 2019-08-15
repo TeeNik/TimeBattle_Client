1589e4c968144e5ec428b12f35b891661d14b629
TeeNik <teenik3315@gmail.com>
2019-08-16 01:14:43 +0300

    user controll

---
+++ Assets/Scripts/UserControll/InputDataController.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum AvailableActions
+{
+    Move,
+}
+
+public class InputDataController : MonoBehaviour
+{
+
+    public Dictionary<int, List<AvailableActions>> CurrentActions;
+
+    
+    private void UpdateActions
+
+}
