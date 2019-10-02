109d52cd45e11ca88c2d014222c528af64c70d8b
TeeNik <teenik3315@gmail.com>
2019-10-03 00:49:35 +0300

    player turn

---
+++ Assets/Scripts/Network/Events/PlayerTurnEvent.cs
@@ -0,0 +1,13 @@
+﻿using Newtonsoft.Json.Linq;
+
+public class PlayerTurnEvent : BaseEventClass
+{
+    public PlayerTurnEvent(string name) : base(name)
+    {
+    }
+
+    protected override void HandleResponseImpl(JObject json)
+    {
+        
+    }
+}
